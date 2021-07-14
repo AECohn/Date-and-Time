45a037522fc0f0d0c7eafd01fafdd46d56fbd802
Aviv Cohn <acohn@biggp.com>
2021-07-14 15:04:10 -0400

    Stable

--- Schedule/Schedule/Schedule_class.cs
+++ Schedule/Schedule/Schedule_class.cs
@@ -17,10 +17,20 @@ namespace Schedule
         public string Scheduled_Time
         
         {
-            get { return scheduled_time; }
+            get
+            {
+                if (Schedule_Set)
+                {
+                    return Read_Schedule();
+                }
+                else
+                {
+                    return "Invalid Format";
+                }
+            }
             set 
             { 
-                Schedule_Set = true; 
+                Schedule_Set = true;
                 try
                 {
                     Formatted_Schedule = DateTime.Parse(value); //JsonWriter should not engage if DateTime.Parse fails (TryParse is not available in VS 2008)
@@ -29,33 +39,32 @@ namespace Schedule
                         Schedule_Writer.Write(JsonConvert.SerializeObject(Formatted_Schedule));
                     }
 
-                    Read_Schedule();
-                    scheduled_time = Recalled_Schedule.ToString("h:mm tt");                    
+                    scheduled_time = value; // The string gets the value it was input with, when the string is read from it presents the value of the stored time
+
                 }
                 catch
                 {
-                    scheduled_time = "invalid format";
-                    Schedule_Set = false; //if schedule is set incorrectly, the scheduling will break, rather than reporting an error and still maintaining the previous schedule
-                }
-
-                
-                    
+                        ErrorLog.Error("Error Setting Schedule");
+                    Schedule_Set = false; //if schedule is set incorrectly, the event will not be sent to Simpl+, rather than reporting an error and still maintaining the previous schedule
+                }                   
             }
 
         }
 
-        public void Read_Schedule()
+        public string Read_Schedule()
         {
              try
             {
-                using (StreamReader Schedule_Reader = new StreamReader("\\user\\Shutdown_Schedule.json"))
+                using (StreamReader Schedule_Reader = new StreamReader("\\user\\Auto_Shutdown_Schedule.json"))
                 {
                     Recalled_Schedule = JsonConvert.DeserializeObject<DateTime>(Schedule_Reader.ReadToEnd());
+                    return Recalled_Schedule.ToString("h:mm tt"); 
                 }
             }
             catch (Exception exception)
             {
                 ErrorLog.Error(exception.Message);
+                return "Read_Error"; //if Schedule Set is True, but there is an issue reading the file
             }
 
         }
