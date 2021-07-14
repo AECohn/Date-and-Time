d45c7edd5ce552e8c8122217796431c654c6e599
Aviv Cohn <acohn@biggp.com>
2021-07-14 13:12:10 -0400

    Schedul writes and reads from json before displaying date

--- Schedule/Schedule/Schedule_class.cs
+++ Schedule/Schedule/Schedule_class.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Text;
 using Crestron.SimplSharp;
-                      				
+using Crestron.SimplSharp.CrestronIO;
+using Newtonsoft.Json;
+           				
 namespace Schedule
 {
     public class Schedule_class
     {
         CTimer Scheduling;
         DateTime Formatted_Schedule;
+        DateTime Recalled_Schedule;
         bool Schedule_Set;
 
         private string scheduled_time;
@@ -17,19 +20,42 @@ namespace Schedule
             get { return scheduled_time; }
             set 
             { 
-                //scheduled_time = value;
-                Schedule_Set = true;
+                Schedule_Set = true; 
                 try
                 {
-                    Formatted_Schedule = DateTime.Parse(value);
-                    scheduled_time = Formatted_Schedule.ToString("h:mm tt");
+                    Formatted_Schedule = DateTime.Parse(value); //JsonWriter should not engage if DateTime.Parse fails (TryParse is not available in VS 2008)
+                    using (StreamWriter Schedule_Writer = new StreamWriter("\\user\\Auto_Shutdown_Schedule.json"))
+                    {
+                        Schedule_Writer.Write(JsonConvert.SerializeObject(Formatted_Schedule));
+                    }
+
+                    Read_Schedule();
+                    scheduled_time = Recalled_Schedule.ToString("h:mm tt");                    
                 }
                 catch
                 {
                     scheduled_time = "invalid format";
-                    Schedule_Set = false;
+                    Schedule_Set = false; //if schedule is set incorrectly, the scheduling will break, rather than reporting an error and still maintaining the previous schedule
                 }
+
+                
                     
+            }
+
+        }
+
+        public void Read_Schedule()
+        {
+             try
+            {
+                using (StreamReader Schedule_Reader = new StreamReader("\\user\\Shutdown_Schedule.json"))
+                {
+                    Recalled_Schedule = JsonConvert.DeserializeObject<DateTime>(Schedule_Reader.ReadToEnd());
+                }
+            }
+            catch (Exception exception)
+            {
+                ErrorLog.Error(exception.Message);
             }
 
         }
@@ -40,10 +66,10 @@ namespace Schedule
             Scheduling = new CTimer(scheduler, this, 0, 5000);
         }
         private void scheduler(object obj)
-                {
-                    if (DateTime.Now.Hour == Formatted_Schedule.Hour && DateTime.Now.Minute == Formatted_Schedule.Minute && Schedule_Set)                    
-                            Update(this, new EventArgs());                    
-                }
+            {
+                if (DateTime.Now.Hour == Recalled_Schedule.Hour && DateTime.Now.Minute == Recalled_Schedule.Minute && Schedule_Set)                    
+                        Update(this, new EventArgs());                    
+            }
         }
 }
 
