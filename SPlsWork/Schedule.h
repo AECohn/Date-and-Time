3ee3f57e84b165e7c915a0e671734a3da12640bb
AECohn <34989255+AECohn@users.noreply.github.com>
2021-10-22 12:02:42 -0400

    adding simple schedule module

---
+++ SPlsWork/Schedule.h
@@ -0,0 +1,22 @@
+namespace Schedule;
+        // class declarations
+         class Schedule_class;
+     class Schedule_class 
+    {
+        // class delegates
+
+        // class events
+        EventHandler Update ( Schedule_class sender, EventArgs e );
+
+        // class functions
+        STRING_FUNCTION Read_Schedule ();
+        SIGNED_LONG_INTEGER_FUNCTION GetHashCode ();
+        STRING_FUNCTION ToString ();
+
+        // class variables
+        INTEGER __class_id__;
+
+        // class properties
+        STRING Scheduled_Time[];
+    };
+
